04b25d2da5a410dcda3ed58ff241c31ce1375c2e
sparky8512 <76499194+sparky8512@users.noreply.github.com>
2020-12-23 07:25:30 -0800

    More example usage, commented out

--- dishDumpStatus.py
+++ dishDumpStatus.py
@@ -14,4 +14,11 @@ with grpc.insecure_channel('192.168.100.1:9200') as channel:
     stub = spacex.api.device.device_pb2_grpc.DeviceStub(channel)
     response = stub.Handle(spacex.api.device.device_pb2.Request(get_status={}))
 
+# Dump everything
 print(response)
+
+## Just the software version
+#print(response.dish_get_status.device_info.software_version)
+
+## Check if connected
+#print("Connected" if response.dish_get_status.state == spacex.api.device.dish_pb2.DishState.CONNECTED else "Not connected")
