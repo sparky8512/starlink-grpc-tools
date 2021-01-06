170dd2daae812cda036408926f9094525da4bbbe
sparky8512 <76499194+sparky8512@users.noreply.github.com>
2021-01-06 11:46:50 -0800

    Reorganize history parsing logic into a separate module
    Moves the parsing logic that will be shared by some upcoming scripts out into a separate module.
    
    There's still just as much duplication between the JSON parser and the grpc parser as there was before, but this should at least prevent further duplication of this logic.
    
    This also adds some proper documentation for what each of the stats means.

--- dishHistoryStats.py
+++ dishHistoryStats.py
@@ -2,7 +2,7 @@
 ######################################################################
 #
 # Equivalent script to parseJsonHistory.py, except integrating the
-# gRPC calls, instead of relying on separatate invocation of grpcurl.
+# gRPC calls, instead of relying on separate invocation of grpcurl.
 #
 # This script examines the most recent samples from the history data
 # and computes several different metrics related to packet loss. By
@@ -10,181 +10,103 @@
 #
 ######################################################################
 
-import grpc
-
-import spacex.api.device.device_pb2
-import spacex.api.device.device_pb2_grpc
-
 import datetime
 import sys
 import getopt
 
-from itertools import chain
+import starlink_grpc
 
-
-fArgError = False
+arg_error = False
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "ahrs:vH")
 except getopt.GetoptError as err:
     print(str(err))
-    fArgError = True
+    arg_error = True
 
 # Default to 1 hour worth of data samples.
-parseSamples = 3600
-fUsage = False
-fVerbose = False
-fParseAll = False
-fHeader = False
-fRunLengths = False
+samples_default = 3600
+samples = samples_default
+print_usage = False
+verbose = False
+parse_all = False
+print_header = False
+run_lengths = False
 
-if not fArgError:
+if not arg_error:
     if len(args) > 0:
-        fArgError = True
+        arg_error = True
     else:
         for opt, arg in opts:
             if opt == "-a":
-                fParseAll = True
+                parse_all = True
             elif opt == "-h":
-                fUsage = True
+                print_usage = True
             elif opt == "-r":
-                fRunLengths = True
+                run_lengths = True
             elif opt == "-s":
-                parseSamples = int(arg)
+                samples = int(arg)
             elif opt == "-v":
-                fVerbose = True
+                verbose = True
             elif opt == "-H":
-                fHeader = True
+                print_header = True
 
-if fUsage or fArgError:
-    print("Usage: "+sys.argv[0]+" [options...]")
+if print_usage or arg_error:
+    print("Usage: " + sys.argv[0] + " [options...]")
     print("Options:")
     print("    -a: Parse all valid samples")
     print("    -h: Be helpful")
     print("    -r: Include ping drop run length stats")
-    print("    -s <num>: Parse <num> data samples, default: "+str(parseSamples))
+    print("    -s <num>: Parse <num> data samples, default: " + str(samples_default))
     print("    -v: Be verbose")
     print("    -H: print CSV header instead of parsing file")
-    sys.exit(1 if fArgError else 0)
+    sys.exit(1 if arg_error else 0)
 
-if fHeader:
-    header = "datetimestamp_utc,samples,total_ping_drop,count_full_ping_drop,count_obstructed,total_obstructed_ping_drop,count_full_obstructed_ping_drop,count_unscheduled,total_unscheduled_ping_drop,count_full_unscheduled_ping_drop"
-    if fRunLengths:
-        header+= ",init_run_fragment,final_run_fragment,"
-        header += ",".join("run_seconds_" + str(x) for x in range(1, 61)) + ","
-        header += ",".join("run_minutes_" + str(x) for x in range(1, 60))
-        header += ",run_minutes_60_or_greater"
-    print(header)
+fields, rl_fields = starlink_grpc.history_ping_field_names()
+
+if print_header:
+    header = ["datetimestamp_utc"]
+    header.extend(fields)
+    if run_lengths:
+        for field in rl_fields:
+            if field.startswith("run_"):
+                header.extend(field + "_" + str(x) for x in range(1, 61))
+            else:
+                header.append(field)
+    print(",".join(header))
     sys.exit(0)
-
-with grpc.insecure_channel('192.168.100.1:9200') as channel:
-    stub = spacex.api.device.device_pb2_grpc.DeviceStub(channel)
-    response = stub.Handle(spacex.api.device.device_pb2.Request(get_history={}))
 
 timestamp = datetime.datetime.utcnow()
 
-historyData = response.dish_get_history
+stats, rl_stats = starlink_grpc.history_ping_stats(-1 if parse_all else samples,
+                                                   verbose)
 
-# 'current' is the count of data samples written to the ring buffer,
-# irrespective of buffer wrap.
-current = int(historyData.current)
-nSamples = len(historyData.pop_ping_drop_rate)
+if stats is None or rl_stats is None:
+    # verbose output already happened, so just bail.
+    sys.exit(1)
 
-if fVerbose:
-    print("current:               " + str(current))
-    print("All samples:           " + str(nSamples))
-
-nSamples = min(nSamples,current)
-
-if fVerbose:
-    print("Valid samples:         " + str(nSamples))
-
-# This is ring buffer offset, so both index to oldest data sample and
-# index to next data sample after the newest one.
-offset = current % nSamples
-
-tot = 0
-totOne = 0
-totUnsched = 0
-totUnschedD = 0
-totUnschedOne = 0
-totObstruct = 0
-totObstructD = 0
-totObstructOne = 0
-
-secondRuns = [0] * 60
-minuteRuns = [0] * 60
-runLength = 0
-initRun = None
-
-if fParseAll or nSamples < parseSamples:
-    parseSamples = nSamples
-
-# Parse the most recent parseSamples-sized set of samples. This will
-# iterate samples in order from oldest to newest, although that's not
-# actually required for the current set of stats being computed below.
-if parseSamples <= offset:
-    sampleRange = range(offset - parseSamples, offset)
+if verbose:
+    print("Parsed samples:        " + str(stats["samples"]))
+    print("Total ping drop:       " + str(stats["total_ping_drop"]))
+    print("Count of drop == 1:    " + str(stats["count_full_ping_drop"]))
+    print("Obstructed:            " + str(stats["count_obstructed"]))
+    print("Obstructed ping drop:  " + str(stats["total_obstructed_ping_drop"]))
+    print("Obstructed drop == 1:  " + str(stats["count_full_obstructed_ping_drop"]))
+    print("Unscheduled:           " + str(stats["count_unscheduled"]))
+    print("Unscheduled ping drop: " + str(stats["total_unscheduled_ping_drop"]))
+    print("Unscheduled drop == 1: " + str(stats["count_full_unscheduled_ping_drop"]))
+    if run_lengths:
+        print("Initial drop run fragment: " + str(rl_stats["init_run_fragment"]))
+        print("Final drop run fragment: " + str(rl_stats["final_run_fragment"]))
+        print("Per-second drop runs:  " + ", ".join(str(x) for x in rl_stats["run_seconds"]))
+        print("Per-minute drop runs:  " + ", ".join(str(x) for x in rl_stats["run_minutes"]))
 else:
-    sampleRange = chain(range(nSamples + offset - parseSamples, nSamples), range(0, offset))
-
-for i in sampleRange:
-    d = historyData.pop_ping_drop_rate[i]
-    tot += d
-    if d >= 1:
-        totOne += d
-        runLength += 1
-    elif runLength > 0:
-        if initRun is None:
-            initRun = runLength
-        else:
-            if runLength <= 60:
-                secondRuns[runLength-1] += runLength
+    csv_data = [timestamp.replace(microsecond=0).isoformat()]
+    csv_data.extend(str(stats[field]) for field in fields)
+    if run_lengths:
+        for field in rl_fields:
+            if field.startswith("run_"):
+                csv_data.extend(str(substat) for substat in rl_stats[field])
             else:
-                minuteRuns[min((runLength-1)//60-1, 59)] += runLength
-        runLength = 0
-    elif initRun is None:
-        initRun = 0
-    if not historyData.scheduled[i]:
-        totUnsched += 1
-        totUnschedD += d
-        if d >= 1:
-            totUnschedOne += d
-    # scheduled=false and obstructed=true do not ever appear to overlap,
-    # but in case they do in the future, treat that as just unscheduled
-    # in order to avoid double-counting it.
-    elif historyData.obstructed[i]:
-        totObstruct += 1
-        totObstructD += d
-        if d >= 1:
-            totObstructOne += d
-
-# If the entire sample set is one big drop run, it will be both initial
-# fragment (continued from prior sample range) and final one (continued
-# to next sample range), but to avoid double-reporting, just call it
-# the initial run.
-if initRun is None:
-    initRun = runLength
-    runLength = 0
-
-if fVerbose:
-    print("Parsed samples:        " + str(parseSamples))
-    print("Total ping drop:       " + str(tot))
-    print("Count of drop == 1:    " + str(totOne))
-    print("Obstructed:            " + str(totObstruct))
-    print("Obstructed ping drop:  " + str(totObstructD))
-    print("Obstructed drop == 1:  " + str(totObstructOne))
-    print("Unscheduled:           " + str(totUnsched))
-    print("Unscheduled ping drop: " + str(totUnschedD))
-    print("Unscheduled drop == 1: " + str(totUnschedOne))
-    if fRunLengths:
-        print("Initial drop run fragment: " + str(initRun))
-        print("Final drop run fragment: " + str(runLength))
-        print("Per-second drop runs:  " + ", ".join(str(x) for x in secondRuns))
-        print("Per-minute drop runs:  " + ", ".join(str(x) for x in minuteRuns))
-else:
-    # NOTE: When changing data output format, also change the -H header printing above.
-    csvData = timestamp.replace(microsecond=0).isoformat() + "," + ",".join(str(x) for x in [parseSamples, tot, totOne, totObstruct, totObstructD, totObstructOne, totUnsched, totUnschedD, totUnschedOne])
-    if fRunLengths:
-        csvData += "," + ",".join(str(x) for x in chain([initRun, runLength], secondRuns, minuteRuns))
-    print(csvData)
+                csv_data.append(str(rl_stats[field]))
+    print(",".join(csv_data))
