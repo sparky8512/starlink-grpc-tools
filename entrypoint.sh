d0fd5a0a2b53837e4ed0bb4b838ab20316963081
Leigh Phillips <neurocis@neurocis.me>
2021-01-10 23:57:06 -0800

    Update entrypoint.sh
    Change to passthrough all args.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ python3 -m grpc_tools.protoc --descriptor_set_in=dish.protoset --python_out=. --
 python3 -m grpc_tools.protoc --descriptor_set_in=dish.protoset --python_out=. --grpc_python_out=. spacex/api/device/dish.proto
 python3 -m grpc_tools.protoc --descriptor_set_in=dish.protoset --python_out=. --grpc_python_out=. spacex/api/device/wifi.proto
 python3 -m grpc_tools.protoc --descriptor_set_in=dish.protoset --python_out=. --grpc_python_out=. spacex/api/device/wifi_config.proto
-/usr/local/bin/python3 $1
+/usr/local/bin/python3 $@
