46f65a62144b783af1c1857ae6b4380adbebf80d
sparky8512 <76499194+sparky8512@users.noreply.github.com>
2021-01-15 18:39:33 -0800

    Implement periodic loop option
    Add an interval timing loop for all the grpc scripts that did not already have one. Organized some of the code into functions in order to facilitate this, which caused some problems with local variables vs global ones, so moved the script code into a proper main() function. Which didn't really solve the access to globals issue, so also moved the mutable state into a class instance.
    
    The interval timer should be relatively robust against time drift due to the loop function running time and/or OS scheduler delay, but is far from perfect.
    
    Retry logic is now in place for both InfluxDB scripts. Retry for dishStatusInflux.py is slightly changed in that a failed write to InfluxDB server will be retried on every interval, rather than waiting for another batch full of data points to write, but this only happens once there is at least a full batch (currently 6) of data points pending. This new behavior matches how the autocommit functionality on SeriesHelper works.
    
    Changed the default behavior of dishStatusInflux.py to not loop, in order to match the other scripts. To get the old behavior, add a '-t 30' option to the command line.
    
    Closes #9

--- dishHistoryInflux.py
+++ dishHistoryInflux.py
@@ -10,169 +10,234 @@
 #
 ######################################################################
 
+import getopt
 import datetime
+import logging
 import os
 import sys
-import getopt
-import logging
-
+import time
 import warnings
+
 from influxdb import InfluxDBClient
 
 import starlink_grpc
 
-arg_error = False
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "ahn:p:rs:vC:D:IP:R:SU:")
-except getopt.GetoptError as err:
-    print(str(err))
-    arg_error = True
+def main():
+    arg_error = False
 
-# Default to 1 hour worth of data samples.
-samples_default = 3600
-samples = samples_default
-print_usage = False
-verbose = False
-run_lengths = False
-host_default = "localhost"
-database_default = "starlinkstats"
-icargs = {"host": host_default, "timeout": 5, "database": database_default}
-rp = None
-
-# For each of these check they are both set and not empty string
-influxdb_host = os.environ.get("INFLUXDB_HOST")
-if influxdb_host:
-    icargs["host"] = influxdb_host
-influxdb_port = os.environ.get("INFLUXDB_PORT")
-if influxdb_port:
-    icargs["port"] = int(influxdb_port)
-influxdb_user = os.environ.get("INFLUXDB_USER")
-if influxdb_user:
-    icargs["username"] = influxdb_user
-influxdb_pwd = os.environ.get("INFLUXDB_PWD")
-if influxdb_pwd:
-    icargs["password"] = influxdb_pwd
-influxdb_db = os.environ.get("INFLUXDB_DB")
-if influxdb_db:
-    icargs["database"] = influxdb_db
-influxdb_rp = os.environ.get("INFLUXDB_RP")
-if influxdb_rp:
-    rp = influxdb_rp
-influxdb_ssl = os.environ.get("INFLUXDB_SSL")
-if influxdb_ssl:
-    icargs["ssl"] = True
-    if influxdb_ssl.lower() == "secure":
-        icargs["verify_ssl"] = True
-    elif influxdb_ssl.lower() == "insecure":
-        icargs["verify_ssl"] = False
-    else:
-        icargs["verify_ssl"] = influxdb_ssl
-
-if not arg_error:
-    if len(args) > 0:
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ahn:p:rs:t:vC:D:IP:R:SU:")
+    except getopt.GetoptError as err:
+        print(str(err))
         arg_error = True
-    else:
-        for opt, arg in opts:
-            if opt == "-a":
-                samples = -1
-            elif opt == "-h":
-                print_usage = True
-            elif opt == "-n":
-                icargs["host"] = arg
-            elif opt == "-p":
-                icargs["port"] = int(arg)
-            elif opt == "-r":
-                run_lengths = True
-            elif opt == "-s":
-                samples = int(arg)
-            elif opt == "-v":
-                verbose = True
-            elif opt == "-C":
-                icargs["ssl"] = True
-                icargs["verify_ssl"] = arg
-            elif opt == "-D":
-                icargs["database"] = arg
-            elif opt == "-I":
-                icargs["ssl"] = True
-                icargs["verify_ssl"] = False
-            elif opt == "-P":
-                icargs["password"] = arg
-            elif opt == "-R":
-                rp = arg
-            elif opt == "-S":
-                icargs["ssl"] = True
-                icargs["verify_ssl"] = True
-            elif opt == "-U":
-                icargs["username"] = arg
 
-if "password" in icargs and "username" not in icargs:
-    print("Password authentication requires username to be set")
-    arg_error = True
+    # Default to 1 hour worth of data samples.
+    samples_default = 3600
+    samples = None
+    print_usage = False
+    verbose = False
+    default_loop_time = 0
+    loop_time = default_loop_time
+    run_lengths = False
+    host_default = "localhost"
+    database_default = "starlinkstats"
+    icargs = {"host": host_default, "timeout": 5, "database": database_default}
+    rp = None
+    flush_limit = 6
 
-if print_usage or arg_error:
-    print("Usage: " + sys.argv[0] + " [options...]")
-    print("Options:")
-    print("    -a: Parse all valid samples")
-    print("    -h: Be helpful")
-    print("    -n <name>: Hostname of InfluxDB server, default: " + host_default)
-    print("    -p <num>: Port number to use on InfluxDB server")
-    print("    -r: Include ping drop run length stats")
-    print("    -s <num>: Number of data samples to parse, default: " + str(samples_default))
-    print("    -v: Be verbose")
-    print("    -C <filename>: Enable SSL/TLS using specified CA cert to verify server")
-    print("    -D <name>: Database name to use, default: " + database_default)
-    print("    -I: Enable SSL/TLS but disable certificate verification (INSECURE!)")
-    print("    -P <word>: Set password for authentication")
-    print("    -R <name>: Retention policy name to use")
-    print("    -S: Enable SSL/TLS using default CA cert")
-    print("    -U <name>: Set username for authentication")
-    sys.exit(1 if arg_error else 0)
-
-logging.basicConfig(format="%(levelname)s: %(message)s")
-
-try:
-    dish_id = starlink_grpc.get_id()
-except starlink_grpc.GrpcError as e:
-    logging.error("Failure getting dish ID: " + str(e))
-    sys.exit(1)
-
-timestamp = datetime.datetime.utcnow()
-
-try:
-    g_stats, pd_stats, rl_stats = starlink_grpc.history_ping_stats(samples, verbose)
-except starlink_grpc.GrpcError as e:
-    logging.error("Failure getting ping stats: " + str(e))
-    sys.exit(1)
-
-all_stats = g_stats.copy()
-all_stats.update(pd_stats)
-if run_lengths:
-    for k, v in rl_stats.items():
-        if k.startswith("run_"):
-            for i, subv in enumerate(v, start=1):
-                all_stats[k + "_" + str(i)] = subv
+    # For each of these check they are both set and not empty string
+    influxdb_host = os.environ.get("INFLUXDB_HOST")
+    if influxdb_host:
+        icargs["host"] = influxdb_host
+    influxdb_port = os.environ.get("INFLUXDB_PORT")
+    if influxdb_port:
+        icargs["port"] = int(influxdb_port)
+    influxdb_user = os.environ.get("INFLUXDB_USER")
+    if influxdb_user:
+        icargs["username"] = influxdb_user
+    influxdb_pwd = os.environ.get("INFLUXDB_PWD")
+    if influxdb_pwd:
+        icargs["password"] = influxdb_pwd
+    influxdb_db = os.environ.get("INFLUXDB_DB")
+    if influxdb_db:
+        icargs["database"] = influxdb_db
+    influxdb_rp = os.environ.get("INFLUXDB_RP")
+    if influxdb_rp:
+        rp = influxdb_rp
+    influxdb_ssl = os.environ.get("INFLUXDB_SSL")
+    if influxdb_ssl:
+        icargs["ssl"] = True
+        if influxdb_ssl.lower() == "secure":
+            icargs["verify_ssl"] = True
+        elif influxdb_ssl.lower() == "insecure":
+            icargs["verify_ssl"] = False
         else:
-            all_stats[k] = v
+            icargs["verify_ssl"] = influxdb_ssl
 
-points = [{
-    "measurement": "spacex.starlink.user_terminal.ping_stats",
-    "tags": {"id": dish_id},
-    "time": timestamp,
-    "fields": all_stats,
-}]
+    if not arg_error:
+        if len(args) > 0:
+            arg_error = True
+        else:
+            for opt, arg in opts:
+                if opt == "-a":
+                    samples = -1
+                elif opt == "-h":
+                    print_usage = True
+                elif opt == "-n":
+                    icargs["host"] = arg
+                elif opt == "-p":
+                    icargs["port"] = int(arg)
+                elif opt == "-r":
+                    run_lengths = True
+                elif opt == "-s":
+                    samples = int(arg)
+                elif opt == "-t":
+                    loop_time = float(arg)
+                elif opt == "-v":
+                    verbose = True
+                elif opt == "-C":
+                    icargs["ssl"] = True
+                    icargs["verify_ssl"] = arg
+                elif opt == "-D":
+                    icargs["database"] = arg
+                elif opt == "-I":
+                    icargs["ssl"] = True
+                    icargs["verify_ssl"] = False
+                elif opt == "-P":
+                    icargs["password"] = arg
+                elif opt == "-R":
+                    rp = arg
+                elif opt == "-S":
+                    icargs["ssl"] = True
+                    icargs["verify_ssl"] = True
+                elif opt == "-U":
+                    icargs["username"] = arg
 
-if "verify_ssl" in icargs and not icargs["verify_ssl"]:
-    # user has explicitly said be insecure, so don't warn about it
-    warnings.filterwarnings("ignore", message="Unverified HTTPS request")
+    if "password" in icargs and "username" not in icargs:
+        print("Password authentication requires username to be set")
+        arg_error = True
 
-influx_client = InfluxDBClient(**icargs)
-try:
-    influx_client.write_points(points, retention_policy=rp)
-    rc = 0
-except Exception as e:
-    logging.error("Failed writing to InfluxDB database: " + str(e))
-    rc = 1
-finally:
-    influx_client.close()
-sys.exit(rc)
+    if print_usage or arg_error:
+        print("Usage: " + sys.argv[0] + " [options...]")
+        print("Options:")
+        print("    -a: Parse all valid samples")
+        print("    -h: Be helpful")
+        print("    -n <name>: Hostname of InfluxDB server, default: " + host_default)
+        print("    -p <num>: Port number to use on InfluxDB server")
+        print("    -r: Include ping drop run length stats")
+        print("    -s <num>: Number of data samples to parse, default: loop interval,")
+        print("              if set, else " + str(samples_default))
+        print("    -t <num>: Loop interval in seconds or 0 for no loop, default: " +
+              str(default_loop_time))
+        print("    -v: Be verbose")
+        print("    -C <filename>: Enable SSL/TLS using specified CA cert to verify server")
+        print("    -D <name>: Database name to use, default: " + database_default)
+        print("    -I: Enable SSL/TLS but disable certificate verification (INSECURE!)")
+        print("    -P <word>: Set password for authentication")
+        print("    -R <name>: Retention policy name to use")
+        print("    -S: Enable SSL/TLS using default CA cert")
+        print("    -U <name>: Set username for authentication")
+        sys.exit(1 if arg_error else 0)
+
+    if samples is None:
+        samples = int(loop_time) if loop_time > 0 else samples_default
+
+    logging.basicConfig(format="%(levelname)s: %(message)s")
+
+    class GlobalState:
+        pass
+
+    gstate = GlobalState()
+    gstate.dish_id = None
+    gstate.points = []
+
+    def conn_error(msg):
+        # Connection errors that happen in an interval loop are not critical
+        # failures, but are interesting enough to print in non-verbose mode.
+        if loop_time > 0:
+            print(msg)
+        else:
+            logging.error(msg)
+
+    def flush_points(client):
+        try:
+            client.write_points(gstate.points, retention_policy=rp)
+            if verbose:
+                print("Data points written: " + str(len(gstate.points)))
+                gstate.points.clear()
+        except Exception as e:
+            conn_error("Failed writing to InfluxDB database: " + str(e))
+            return 1
+
+        return 0
+
+    def loop_body(client):
+        if gstate.dish_id is None:
+            try:
+                gstate.dish_id = starlink_grpc.get_id()
+                if verbose:
+                    print("Using dish ID: " + gstate.dish_id)
+            except starlink_grpc.GrpcError as e:
+                conn_error("Failure getting dish ID: " + str(e))
+                return 1
+
+        timestamp = datetime.datetime.utcnow()
+
+        try:
+            g_stats, pd_stats, rl_stats = starlink_grpc.history_ping_stats(samples, verbose)
+        except starlink_grpc.GrpcError as e:
+            conn_error("Failure getting ping stats: " + str(e))
+            return 1
+
+        all_stats = g_stats.copy()
+        all_stats.update(pd_stats)
+        if run_lengths:
+            for k, v in rl_stats.items():
+                if k.startswith("run_"):
+                    for i, subv in enumerate(v, start=1):
+                        all_stats[k + "_" + str(i)] = subv
+                else:
+                    all_stats[k] = v
+
+        gstate.points.append({
+            "measurement": "spacex.starlink.user_terminal.ping_stats",
+            "tags": {
+                "id": gstate.dish_id
+            },
+            "time": timestamp,
+            "fields": all_stats,
+        })
+        if verbose:
+            print("Data points queued: " + str(len(gstate.points)))
+
+        if len(gstate.points) >= flush_limit:
+            return flush_points(client)
+
+        return 0
+
+    if "verify_ssl" in icargs and not icargs["verify_ssl"]:
+        # user has explicitly said be insecure, so don't warn about it
+        warnings.filterwarnings("ignore", message="Unverified HTTPS request")
+
+    influx_client = InfluxDBClient(**icargs)
+    try:
+        next_loop = time.monotonic()
+        while True:
+            rc = loop_body(influx_client)
+            if loop_time > 0:
+                now = time.monotonic()
+                next_loop = max(next_loop + loop_time, now)
+                time.sleep(next_loop - now)
+            else:
+                break
+    finally:
+        if gstate.points:
+            rc = flush_points(influx_client)
+        influx_client.close()
+
+    sys.exit(rc)
+
+
+if __name__ == '__main__':
+    main()
--- dishStatusMqtt.py
+++ dishStatusMqtt.py
@@ -3,14 +3,15 @@
 #
 # Publish Starlink user terminal status info to a MQTT broker.
 #
-# This script pulls the current status once and publishes it to the
-# specified MQTT broker.
+# This script pulls the current status and publishes it to the
+# specified MQTT broker either once or in a periodic loop.
 #
 ######################################################################
 
-import sys
 import getopt
 import logging
+import sys
+import time
 
 try:
     import ssl
@@ -18,116 +19,170 @@ try:
 except ImportError:
     ssl_ok = False
 
-import paho.mqtt.publish
-
 import grpc
+import paho.mqtt.publish
 
 import spacex.api.device.device_pb2
 import spacex.api.device.device_pb2_grpc
 
-arg_error = False
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "hn:p:C:ISP:U:")
-except getopt.GetoptError as err:
-    print(str(err))
-    arg_error = True
+def main():
+    arg_error = False
 
-print_usage = False
-host_default = "localhost"
-mqargs = {"hostname": host_default}
-username = None
-password = None
-
-if not arg_error:
-    if len(args) > 0:
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hn:p:t:vC:ISP:U:")
+    except getopt.GetoptError as err:
+        print(str(err))
         arg_error = True
-    else:
-        for opt, arg in opts:
-            if opt == "-h":
-                print_usage = True
-            elif opt == "-n":
-                mqargs["hostname"] = arg
-            elif opt == "-p":
-                mqargs["port"] = int(arg)
-            elif opt == "-C":
-                mqargs["tls"] = {"ca_certs": arg}
-            elif opt == "-I":
-                if ssl_ok:
-                    mqargs["tls"] = {"cert_reqs": ssl.CERT_NONE}
-                else:
-                    print("No SSL support found")
-                    sys.exit(1)
-            elif opt == "-P":
-                password = arg
-            elif opt == "-S":
-                mqargs["tls"] = {}
-            elif opt == "-U":
-                username = arg
 
-if username is None and password is not None:
-    print("Password authentication requires username to be set")
-    arg_error = True
+    print_usage = False
+    verbose = False
+    default_loop_time = 0
+    loop_time = default_loop_time
+    host_default = "localhost"
+    mqargs = {"hostname": host_default}
+    username = None
+    password = None
 
-if print_usage or arg_error:
-    print("Usage: " + sys.argv[0] + " [options...]")
-    print("Options:")
-    print("    -h: Be helpful")
-    print("    -n <name>: Hostname of MQTT broker, default: " + host_default)
-    print("    -p <num>: Port number to use on MQTT broker")
-    print("    -C <filename>: Enable SSL/TLS using specified CA cert to verify broker")
-    print("    -I: Enable SSL/TLS but disable certificate verification (INSECURE!)")
-    print("    -P: Set password for username/password authentication")
-    print("    -S: Enable SSL/TLS using default CA cert")
-    print("    -U: Set username for authentication")
-    sys.exit(1 if arg_error else 0)
+    if not arg_error:
+        if len(args) > 0:
+            arg_error = True
+        else:
+            for opt, arg in opts:
+                if opt == "-h":
+                    print_usage = True
+                elif opt == "-n":
+                    mqargs["hostname"] = arg
+                elif opt == "-p":
+                    mqargs["port"] = int(arg)
+                elif opt == "-t":
+                    loop_time = float(arg)
+                elif opt == "-v":
+                    verbose = True
+                elif opt == "-C":
+                    mqargs["tls"] = {"ca_certs": arg}
+                elif opt == "-I":
+                    if ssl_ok:
+                        mqargs["tls"] = {"cert_reqs": ssl.CERT_NONE}
+                    else:
+                        print("No SSL support found")
+                        sys.exit(1)
+                elif opt == "-P":
+                    password = arg
+                elif opt == "-S":
+                    mqargs["tls"] = {}
+                elif opt == "-U":
+                    username = arg
 
-logging.basicConfig(format="%(levelname)s: %(message)s")
+    if username is None and password is not None:
+        print("Password authentication requires username to be set")
+        arg_error = True
 
-try:
-    with grpc.insecure_channel("192.168.100.1:9200") as channel:
-        stub = spacex.api.device.device_pb2_grpc.DeviceStub(channel)
-        response = stub.Handle(spacex.api.device.device_pb2.Request(get_status={}))
-except grpc.RpcError:
-    logging.error("Failed getting status info")
-    sys.exit(1)
+    if print_usage or arg_error:
+        print("Usage: " + sys.argv[0] + " [options...]")
+        print("Options:")
+        print("    -h: Be helpful")
+        print("    -n <name>: Hostname of MQTT broker, default: " + host_default)
+        print("    -p <num>: Port number to use on MQTT broker")
+        print("    -t <num>: Loop interval in seconds or 0 for no loop, default: " +
+              str(default_loop_time))
+        print("    -v: Be verbose")
+        print("    -C <filename>: Enable SSL/TLS using specified CA cert to verify broker")
+        print("    -I: Enable SSL/TLS but disable certificate verification (INSECURE!)")
+        print("    -P: Set password for username/password authentication")
+        print("    -S: Enable SSL/TLS using default CA cert")
+        print("    -U: Set username for authentication")
+        sys.exit(1 if arg_error else 0)
 
-status = response.dish_get_status
+    if username is not None:
+        mqargs["auth"] = {"username": username}
+        if password is not None:
+            mqargs["auth"]["password"] = password
 
-# More alerts may be added in future, so rather than list them individually,
-# build a bit field based on field numbers of the DishAlerts message.
-alert_bits = 0
-for alert in status.alerts.ListFields():
-    alert_bits |= (1 if alert[1] else 0) << (alert[0].number - 1)
+    logging.basicConfig(format="%(levelname)s: %(message)s")
 
-topic_prefix = "starlink/dish_status/" + status.device_info.id + "/"
-msgs = [(topic_prefix + "hardware_version", status.device_info.hardware_version, 0, False),
-        (topic_prefix + "software_version", status.device_info.software_version, 0, False),
-        (topic_prefix + "state", spacex.api.device.dish_pb2.DishState.Name(status.state), 0, False),
-        (topic_prefix + "uptime", status.device_state.uptime_s, 0, False),
-        (topic_prefix + "snr", status.snr, 0, False),
-        (topic_prefix + "seconds_to_first_nonempty_slot", status.seconds_to_first_nonempty_slot, 0, False),
-        (topic_prefix + "pop_ping_drop_rate", status.pop_ping_drop_rate, 0, False),
-        (topic_prefix + "downlink_throughput_bps", status.downlink_throughput_bps, 0, False),
-        (topic_prefix + "uplink_throughput_bps", status.uplink_throughput_bps, 0, False),
-        (topic_prefix + "pop_ping_latency_ms", status.pop_ping_latency_ms, 0, False),
-        (topic_prefix + "alerts", alert_bits, 0, False),
-        (topic_prefix + "fraction_obstructed", status.obstruction_stats.fraction_obstructed, 0, False),
-        (topic_prefix + "currently_obstructed", status.obstruction_stats.currently_obstructed, 0, False),
-        # While the field name for this one implies it covers 24 hours, the
-        # empirical evidence suggests it only covers 12 hours. It also resets
-        # on dish reboot, so may not cover that whole period. Rather than try
-        # to convey that complexity in the topic label, just be a bit vague:
-        (topic_prefix + "seconds_obstructed", status.obstruction_stats.last_24h_obstructed_s, 0, False),
-        (topic_prefix + "wedges_fraction_obstructed", ",".join(str(x) for x in status.obstruction_stats.wedge_abs_fraction_obstructed), 0, False)]
+    class GlobalState:
+        pass
 
-if username is not None:
-    mqargs["auth"] = {"username": username}
-    if password is not None:
-        mqargs["auth"]["password"] = password
+    gstate = GlobalState()
+    gstate.dish_id = None
 
-try:
-    paho.mqtt.publish.multiple(msgs, client_id=status.device_info.id, **mqargs)
-except Exception as e:
-    logging.error("Failed publishing to MQTT broker: " + str(e))
-    sys.exit(1)
+    def conn_error(msg):
+        # Connection errors that happen in an interval loop are not critical
+        # failures, but are interesting enough to print in non-verbose mode.
+        if loop_time > 0:
+            print(msg)
+        else:
+            logging.error(msg)
+
+    def loop_body():
+        try:
+            with grpc.insecure_channel("192.168.100.1:9200") as channel:
+                stub = spacex.api.device.device_pb2_grpc.DeviceStub(channel)
+                response = stub.Handle(spacex.api.device.device_pb2.Request(get_status={}))
+
+            status = response.dish_get_status
+
+            # More alerts may be added in future, so rather than list them individually,
+            # build a bit field based on field numbers of the DishAlerts message.
+            alert_bits = 0
+            for alert in status.alerts.ListFields():
+                alert_bits |= (1 if alert[1] else 0) << (alert[0].number - 1)
+
+            gstate.dish_id = status.device_info.id
+            topic_prefix = "starlink/dish_status/" + gstate.dish_id + "/"
+            msgs = [
+                (topic_prefix + "hardware_version", status.device_info.hardware_version, 0, False),
+                (topic_prefix + "software_version", status.device_info.software_version, 0, False),
+                (topic_prefix + "state", spacex.api.device.dish_pb2.DishState.Name(status.state), 0, False),
+                (topic_prefix + "uptime", status.device_state.uptime_s, 0, False),
+                (topic_prefix + "snr", status.snr, 0, False),
+                (topic_prefix + "seconds_to_first_nonempty_slot", status.seconds_to_first_nonempty_slot, 0, False),
+                (topic_prefix + "pop_ping_drop_rate", status.pop_ping_drop_rate, 0, False),
+                (topic_prefix + "downlink_throughput_bps", status.downlink_throughput_bps, 0, False),
+                (topic_prefix + "uplink_throughput_bps", status.uplink_throughput_bps, 0, False),
+                (topic_prefix + "pop_ping_latency_ms", status.pop_ping_latency_ms, 0, False),
+                (topic_prefix + "alerts", alert_bits, 0, False),
+                (topic_prefix + "fraction_obstructed", status.obstruction_stats.fraction_obstructed, 0, False),
+                (topic_prefix + "currently_obstructed", status.obstruction_stats.currently_obstructed, 0, False),
+                # While the field name for this one implies it covers 24 hours, the
+                # empirical evidence suggests it only covers 12 hours. It also resets
+                # on dish reboot, so may not cover that whole period. Rather than try
+                # to convey that complexity in the topic label, just be a bit vague:
+                (topic_prefix + "seconds_obstructed", status.obstruction_stats.last_24h_obstructed_s, 0, False),
+                (topic_prefix + "wedges_fraction_obstructed", ",".join(str(x) for x in status.obstruction_stats.wedge_abs_fraction_obstructed), 0, False),
+            ]
+        except grpc.RpcError:
+            if gstate.dish_id is None:
+                conn_error("Dish unreachable and ID unknown, so not recording state")
+                return 1
+            if verbose:
+                print("Dish unreachable")
+            topic_prefix = "starlink/dish_status/" + gstate.dish_id + "/"
+            msgs = [(topic_prefix + "state", "DISH_UNREACHABLE", 0, False)]
+
+        try:
+            paho.mqtt.publish.multiple(msgs, client_id=gstate.dish_id, **mqargs)
+            if verbose:
+                print("Successfully published to MQTT broker")
+        except Exception as e:
+            conn_error("Failed publishing to MQTT broker: " + str(e))
+            return 1
+
+        return 0
+
+    next_loop = time.monotonic()
+    while True:
+        rc = loop_body()
+        if loop_time > 0:
+            now = time.monotonic()
+            next_loop = max(next_loop + loop_time, now)
+            time.sleep(next_loop - now)
+        else:
+            break
+
+    sys.exit(rc)
+
+
+if __name__ == '__main__':
+    main()
